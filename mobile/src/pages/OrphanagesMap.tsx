8af32e49a7773eaa36031cf35fb7841a7b9a5e19
vitor-risso <vitor.risso13@gmail.com>
2020-10-16 20:50:42 -0300

    nlw3 finalizada, create orphaanges ok

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 import mapMarker from '../images/map-marker.png';
@@ -20,11 +20,11 @@ export default function OrphanagesMap(){
     const navigation = useNavigation() 
     const [orphanages, setOrphanages] = useState<OrphanageItem[]>([])
 
-    useEffect(() => {
+    useFocusEffect(() => {
       api.get('/orphanages').then(response =>{
         setOrphanages(response.data)
       })
-    },[])
+    })
 
     function handleNavigateToOrphanageDetails(id:number){
         navigation.navigate('OrphanageDetails', { id })

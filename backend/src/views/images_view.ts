8a087f840921c6b607ebbc2f9ca6abe0ff7e92f0
vitor-risso <vitor.risso13@gmail.com>
2020-10-16 19:03:29 -0300

    orphanage details ok

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
     render(image: Image){
         return{
             id: image.id,
-            url: `http://localhost:8080/uploads/${image.path}`
+            url: `http://192.168.0.36:8080/uploads/${image.path}`
         }
     },
 
